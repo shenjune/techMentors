cb9093776c2d2d3a1707614cb31fd7d002d5a0ee
CrypticWoodWhite <crypticwoodwhite@gmail.com>
2019-10-06 12:32:12 -0700

    all routes work

--- models/mentor.js
+++ models/mentor.js
@@ -1,17 +1,31 @@
 module.exports = function(sequelize, DataTypes) {
     var Mentor = sequelize.define("Mentors", {
       // Giving the Mentee model a name of type STRING
-      mentor_name: DataTypes.STRING,
-      image_id: DataTypes.STRING,
+      mentor_name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      image_id: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
       primary_help:  {
-        type: DataTypes.ENUM('Class Assignment', 'Specific Coding Problem', 'Job Prep/Interview Questions'), 
-        default: 'Select'},
-      description: DataTypes.TEXT,
+        type: DataTypes.ENUM('Select', 'Class Assignment', 'Specific Coding Problem', 'Job Prep/Interview Questions'), 
+        default: 'Select',
+        allowNull: false
+      },
+      description: {
+        type: DataTypes.TEXT,
+        allowNull: true
+      },
       institute_graduated:  {
-      type: DataTypes.ENUM('University of Washington Coding Boot Camp', 'CodeFellows Seattle', 'Galvanize Coding Bootcamp-Seattle', 'General Assembly-Seattle'),
-      default: 'Select'},
+        type: DataTypes.ENUM('Select', 'University of Washington Coding Boot Camp', 'CodeFellows Seattle', 'Galvanize Coding Bootcamp-Seattle', 'General Assembly-Seattle'),
+        default: 'Select',
+        allowNull: true
+      },
       year_graduated: {
           type: DataTypes.INTEGER,
+          allowNull: false
           // validate: {function(input) {
           //   if (input < 2020){
           //       return true 
@@ -21,10 +35,15 @@ module.exports = function(sequelize, DataTypes) {
           //   }
           // }}
       },
-      fav_assignment: DataTypes.STRING,
+      fav_assignment: {
+        type: DataTypes.STRING,
+        allowNull: true
+      },
       time_zone:  {
-        type: DataTypes.ENUM('UTC-11: Samoa Standard Time', 'UTC-10 Hawaii-Aleutian Standard Time (HST)', 'UTC-9 Alaska Standard Time (AKST)', 'UTC-8: Pacific Standard Time (PST)', 'UTC-7: Mountain Standard Time (MST)', 'UTC-6: Central Standard Time (CST)', 'UTC-5: Eastern Standard Time (EST)', 'UTC-4: Atlantic Standard Time (AST)', 'UTC+10: Chamorro Standard Time'),
-        default: 'Select'}
-    })
+        type: DataTypes.ENUM('Select', 'UTC-11: Samoa Standard Time', 'UTC-10 Hawaii-Aleutian Standard Time (HST)', 'UTC-9 Alaska Standard Time (AKST)', 'UTC-8: Pacific Standard Time (PST)', 'UTC-7: Mountain Standard Time (MST)', 'UTC-6: Central Standard Time (CST)', 'UTC-5: Eastern Standard Time (EST)', 'UTC-4: Atlantic Standard Time (AST)', 'UTC+10: Chamorro Standard Time'),
+        default: 'Select',
+        allowNull: false
+      }
+    });
     return Mentor;
 };
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -9,81 +9,64 @@
 var db = require("../models");
 
 module.exports = function(app) {
-    app.get("/api/mentors", function(req, res) {
-      //THIS ROUTE WORKS! DON'T TOUCH IT!!!!
-      // Here we add an "include" property to our options in our findAll query
-      // We set the value to an array of the models we want to include in a left outer join
-      // In this case, just db.Post
-      db.Mentors.findAll({
-        //include: [db.Post]
+  app.get("/api/mentors", function(req, res) {
+    //THIS ROUTE WORKS! DON'T TOUCH IT!!!!
+    db.Mentors.findAll({
       }).then(function(dbMentor) {
         res.json(dbMentor);
       });
+  });
+
+  app.get("/api/primary_help/:primary_help", function(req, res) {
+    // WORKS
+    db.Mentors.findAll({
+      where: {
+        primary_help: req.params.primary_help
+      }
+    }).then(function(dbMentor) {
+      res.json(dbMentor);
     });
+  });
+    
+  app.get("/api/time_zone/:time_zone", function(req, res) {
+    //WORKS
+    db.Mentors.findAll({
+      where: {
+        time_zone: req.params.time_zone
+      }
+    }).then(function(dbMentor) {
+      res.json(dbMentor);
+    });
+  });
 
-    app.get("/api/mentors/:primary_help", function(req, res) {
-       //Not sure if this is right
-        db.Mentors.findAll({
-          where: {
-            primary_help: req.params.primary_help
-          }
-          //include: [db.Post]
-        }).then(function(dbMentor) {
-          res.json(dbMentor);
-        });
-      });
-    
-      app.get("/api/mentors", function(req, res) {
-        //Not sure if this is right
-         db.Mentors.findAll({
-           where: {
-             time_zone: req.query.time_zone
-           }
-           //include: [db.Post]
-         }).then(function(dbMentor) {
-           res.json(dbMentor);
-         });
-       });
+  app.get("/api/primaryhelp_timezone/:primary_help/:time_zone", function(req, res) {
+    //WORKS
+    db.Mentors.findAll({
+      where: {
+        primary_help: req.params.primary_help,
+        time_zone: req.params.time_zone
+      }
+    }).then(function(dbMentor) {
+      res.json(dbMentor);
+    });
+  });
 
-      //  app.get("/api/mentors/:time_zone", function(req, res) {
-      //   //Not sure if this is right
-      //    db.Mentors.findAll({
-      //      where: {
-      //        time_zone: req.params.time_zone
-      //      }
-      //      //include: [db.Post]
-      //    }).then(function(dbMentor) {
-      //      res.json(dbMentor);
-      //    });
-      //  });
-    
-      //Post Mentor Information
-      //replace mentors with "AddMentor" if something goes wrong
-      app.post("/api/mentors", function(req, res) {
-          //console.log(req.body);
-        db.Mentors.create(
-          req.body
-        // mentor_name: 'Trae Shanks',
-        // primary_help: 'Specific Coding Problem',
-        // description: 'I am cool ',
-        // institute_graduated: 'University of Washington Coding Boot Camp',
-        // year_graduated: 2019,
-        // time_zone: 'UTC-8: Pacific Standard Time (PST)'
-     )
-        .then(function (data) {
-            res.json(data)
-        })
-        //take out yellow curly bracket and put at bottom if you want to keep the DELETE function
-      });
-    
-    //   The DELETE ROUTE WORKS. DON'T TOUCH IT!!!
-    //Ability to delete Mentor information (not sure if this is correct, might not keep)
-      app.delete("/api/mentors", function(req, res) {
-        db.Mentors.destroy({
-              where: {
-              id: req.query.id
-          }
-          }).then(function(dbMentor) {
-              res.json(dbMentor);
-           });
-         })};
+  //Post Mentor Information
+  app.post("/api/mentors", function (req, res) {
+    db.Mentors.create(req.body).then(function(dbMentor) {
+      res.json(dbMentor);
+    });
+  })
+
+//   The DELETE ROUTE WORKS. DON'T TOUCH IT!!!
+//Ability to delete Mentor information (not sure if this is correct, might not keep)
+  app.delete("/api/mentors/:id", function(req, res) {
+    db.Mentors.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then(function(dbMentor) {
+      res.json(dbMentor);
+    });
+  })
+};
