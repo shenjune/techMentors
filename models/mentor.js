c9a9104087eaec704a3adc6a7194e56a12b48d78
Jenn June <jennjuneux@gmail.com>
2019-10-13 11:09:12 -0700

    uploading files for Catherine review

--- models/mentor.js
+++ models/mentor.js
@@ -5,13 +5,13 @@ module.exports = function(sequelize, DataTypes) {
       image_id: DataTypes.STRING,
       primary_help:  {
         type: DataTypes.ENUM('Class Assignment', 'Specific Coding Problem', 'Job Prep/Interview Questions'), 
-        default: 'Select'},
+        },
       description: DataTypes.TEXT,
       institute_graduated:  {
-      type: DataTypes.ENUM('University of Washington Coding Boot Camp', 'CodeFellows Seattle', 'Galvanize Coding Bootcamp-Seattle', 'General Assembly-Seattle'),
-      default: 'Select'},
-      year_graduated: {
-          type: DataTypes.INTEGER,
+      type: DataTypes.ENUM('Select','Trilogy Education Services', 'CodeFellows', 'Galvanize Coding Bootcamp', 'General Assembly', 'Not listed'),
+      },
+      //year_graduated: {
+          //type: DataTypes.INTEGER,
           // validate: {function(input) {
           //   if (input < 2020){
           //       return true 
@@ -20,10 +20,10 @@ module.exports = function(sequelize, DataTypes) {
           //       return false;
           //   }
           // }}
-      },
-      fav_assignment: DataTypes.STRING,
+      //},
+      //fav_assignment: DataTypes.STRING,
       time_zone:  {
-        type: DataTypes.ENUM('UTC-11: Samoa Standard Time', 'UTC-10 Hawaii-Aleutian Standard Time (HST)', 'UTC-9 Alaska Standard Time (AKST)', 'UTC-8: Pacific Standard Time (PST)', 'UTC-7: Mountain Standard Time (MST)', 'UTC-6: Central Standard Time (CST)', 'UTC-5: Eastern Standard Time (EST)', 'UTC-4: Atlantic Standard Time (AST)', 'UTC+10: Chamorro Standard Time'),
+        type: DataTypes.ENUM('Select','UTC-11: Samoa Standard Time', 'UTC-10 Hawaii-Aleutian Standard Time (HST)', 'UTC-9 Alaska Standard Time (AKST)', 'UTC-8: Pacific Standard Time (PST)', 'UTC-7: Mountain Standard Time (MST)', 'UTC-6: Central Standard Time (CST)', 'UTC-5: Eastern Standard Time (EST)', 'UTC-4: Atlantic Standard Time (AST)', 'UTC+10: Chamorro Standard Time'),
         default: 'Select'}
     })
     return Mentor;
